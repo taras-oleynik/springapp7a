6bb136f9728bd560fb7b2535b71e93a8b3b91ff4
taraso <Taras.Oliinyk@kstt.ua>
2019-07-22 17:26:18 +0300

    (Bean Lifecycle). Init, Destroy и Factory

--- src/main/java/com/tar/spring/RockMusic.java
+++ src/main/java/com/tar/spring/RockMusic.java
@@ -1,6 +1,23 @@
 package com.tar.spring;
 
 public class RockMusic implements Music {
+
+    private RockMusic() {
+    }
+
+    public static RockMusic getRockMusic() {
+        return new RockMusic();
+    }
+
+    //doMyInit doDestroy should not have any args
+    public void doMyInit() {
+        System.out.println("do init");
+    }
+
+    public void doDestroy() {
+        System.out.println("do destraction");
+    }
+
     @Override
     public String getSong() {
         return "We wil rock you";
--- src/main/java/com/tar/spring/TestSpring.java
+++ src/main/java/com/tar/spring/TestSpring.java
@@ -9,26 +9,31 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 
                 "applicationContext.xml");
+
+        RockMusic rockMusic = context.getBean("musicBean", RockMusic.class);
+        System.out.println(rockMusic.getSong());
+
+
         //bean id="testBean" and class obj we want to receive
         //Music music = context.getBean("musicBean", Music.class); we made this bean in XML
         //inject dependancy
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
         //Get from context Constructors injections
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-       // MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
+       /* MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);*/
+        // MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
         //musicPlayer.playMusic();
 
         //System.out.println(musicPlayer.getName());
         //System.out.println(musicPlayer.getVolume());
         //close app after the finish
 // by default single tone that's why it trigers the same object, if scope="prototype"
-        boolean compare = firstMusicPlayer== secondMusicPlayer;
+       /* boolean compare = firstMusicPlayer== secondMusicPlayer;
         firstMusicPlayer.setVolume(45);
         System.out.println(firstMusicPlayer.getVolume());
         System.out.println(secondMusicPlayer.getVolume());
-        System.out.println(compare);
+        System.out.println(compare);*/
         context.close();
     }
 }
